00e0e53dcab4293fae9af1583e5f962de0bade26
RITISAHU <109952605+RITISAHU@users.noreply.github.com>
2022-07-29 21:44:55 +0530

    Add files via upload

---
+++ project 2/app.py
@@ -0,0 +1,30 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+
+app = Flask(__name__)
+model = pickle.load(open('linearregression2.pkl','rb')) 
+
+@app.route('/')
+def home():
+  
+    return render_template("index.html")
+  
+@app.route('/predict',methods=['GET'])
+def predict():
+    
+    
+    '''
+    For rendering results on HTML GUI
+    '''
+    exp = float(request.args.get('exp'))
+    
+    prediction = model.predict([[exp]])
+    
+        
+    return render_template('index.html', prediction_text='Regression Model  has predicted Price for given requirements is : {}'.format(prediction))
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
